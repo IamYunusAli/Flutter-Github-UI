75d181a6a4ad27410238622a5b23e9d3cf33e539
iamyunusali <aliyunus291@gmail.com>
2020-05-09 22:49:49 +0300

    added app bar and body

--- lib/notificationpage.dart
+++ lib/notificationpage.dart
@@ -8,6 +8,32 @@ class Pagenotification extends StatefulWidget {
 class _PagenotificationState extends State<Pagenotification> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: GAppbarColor,
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+          child: CircleAvatar(
+            maxRadius: 0.1,
+            backgroundImage: AssetImage('assets/yunus.jpg'),
+          ),
+        ),
+        title: Text(
+          'Inbox',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          Icon(
+            Icons.language,
+            color: GIconColor,
+          ),
+        ],
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+        height: MediaQuery.of(context).size.height / 2,
+        color: GBodyColor,
+      ),
+    );
   }
 }
