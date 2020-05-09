67cf91499fed41954028ba49e7a3369ef7af0cbf
iamyunusali <aliyunus291@gmail.com>
2020-05-09 22:40:07 +0300

    Additional colors

--- lib/utils.dart
+++ lib/utils.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 // colors
 const Color GAppbarColor = Color(0xFF17181A);
 const Color GButtonColor = Color(0xFF242529);
-const Color GBottomAppBarColor = Color(0xFF242529);
+const Color GBottomAppBarColor = Color(0xFF17181A);
+const Color GBodyColor = Color(0xFF17181A);
 const Color GIconColor = Color(0xFF0d6dd9);
