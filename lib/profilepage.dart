f0772609ff4450f3581dd374480e55d8f40dbd1f
iamyunusali <aliyunus291@gmail.com>
2020-05-10 15:45:50 +0300

    finished appbar

--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'utils.dart';
 
@@ -10,7 +11,34 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: GBodyColor,
       appBar: AppBar(
+        backgroundColor: GAppbarColor,
+        leading: Icon(
+          Icons.arrow_back,
+          color: GIconColor,
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.share,
+                color: GIconColor,
+              ),
+              onPressed: null),
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: GIconColor,
+            ),
+            onPressed: null,
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Container(
+          color: GBodyColor,
+        ),
       ),
     );
   }
