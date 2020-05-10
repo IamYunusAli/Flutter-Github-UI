d718886db476d911eff9e7ba55e0dc72c5aa7969
iamyunusali <aliyunus291@gmail.com>
2020-05-10 15:14:00 +0300

    Added new color

--- lib/utils.dart
+++ lib/utils.dart
@@ -6,3 +6,4 @@ const Color GButtonColor = Color(0xFF242529);
 const Color GBottomAppBarColor = Color(0xFF17181A);
 const Color GBodyColor = Color(0xFF17181A);
 const Color GIconColor = Color(0xFF0d6dd9);
+const Color GTextColor = Color(0xFFFFFFFF);
