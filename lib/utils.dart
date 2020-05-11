2b98cf6bded3a4e28fa9277abce02e93735dca75
iamyunusali <aliyunus291@gmail.com>
2020-05-11 23:02:33 +0300

    light colors

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,9 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-// colors
+// Dark colors
 const Color GAppbarColor = Color(0xFF17181A);
 const Color GButtonColor = Color(0xFF242529);
 const Color GBottomAppBarColor = Color(0xFF17181A);
 const Color GBodyColor = Color(0xFF17181A);
 const Color GIconColor = Color(0xFF0d6dd9);
 const Color GTextColor = Color(0xFFFFFFFF);
+
+// light colors
+
+const Color GLTextColor = Color(0xFF17181A);
+const Color GLIconColor = Color(0xFF0d6dd9);
+const Color GLUIconColor = Color(0xFFFF000);
+const Color GLBodyColor = Color(0xFFFFFFfF);
+const Color GLBottomAppBarColor = Color(0xFFFFFFfF);
