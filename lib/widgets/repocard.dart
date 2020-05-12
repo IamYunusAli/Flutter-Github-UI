eb89ab6f1f7aca46ac272d07e64a95c26fd73169
iamyunusali <aliyunus291@gmail.com>
2020-05-12 15:05:22 +0300

    repo card

---
+++ lib/widgets/repocard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Widget _buildRepoCard(
+  String imgpath,
+  String username,
+  String title,
+  String desc,
+  String lang,
+  int star,
+) {
+  return Scaffold();
+}
