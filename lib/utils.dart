eb345b730b9156db5c31b3b6157e77f48d396cc9
iamyunusali <aliyunus291@gmail.com>
2020-05-16 23:43:16 +0300

    common color added

--- lib/utils.dart
+++ lib/utils.dart
@@ -16,3 +16,11 @@ const Color GLIconColor = Color(0xFF0d6dd9);
 const Color GLUIconColor = Color(0xFFFF000);
 const Color GLBodyColor = Color(0xFFFFFFfF);
 const Color GLBottomAppBarColor = Color(0xFFFFFFfF);
+
+// common colors
+
+const Color CIconColor1 = Color(0xff12f150);
+const Color CIconColor2 = Color(0xff0c91f7);
+const Color CIconColor3 = Color(0xff8b5cd5);
+const Color CIconColor4 = Color(0xfff9812a);
+const Color CIconTextColor = Color(0xffffffff);
